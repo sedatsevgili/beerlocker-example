515aa4fc26d0f895158417d1e144e8b3b3caafd2
sedat sevgili <sedatsevgili@gmail.com>
2016-09-06 18:17:06 +0300

    all crud actions are moved to controller

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
-var Beer = require('./models/beer');
+var beerController = require('./controllers/beer');
 var app = express();
-var port = process.env.PORT || 3000;
 var router = express.Router();
 
 app.use(bodyParser.urlencoded({
@@ -12,74 +11,15 @@ app.use(bodyParser.urlencoded({
 
 mongoose.connect('mongodb://localhost:27017/beerlocker');
 
-router.get('/', function(req, res) {
-	res.json({message: 'You are running dangerously low on beer!!'})
-});
+router.route('/beers')
+	.post(beerController.postBeers)
+	.get(beerController.getBeers);
 
-var beersRoute = router.route('/beers');
-var beerRoute = router.route('/beers/:beer_id');
-
-beersRoute.post(function(req, res) {
-	// create a new beer
-	var beer = new Beer();
-
-	beer.name = req.body.name;
-	beer.type = req.body.type;
-	beer.quantity = req.body.quantity;
-
-	beer.save(function(err) {
-		if(err) {
-			return res.send(err);
-		}
-		res.json({message:'Beer added to the locker!', data: beer});
-	});
-});
-
-beersRoute.get(function(req, res) {
-	Beer.find(function(err, beers) {
-		if(err) {
-			res.send(err);
-		}
-		res.json(beers);
-	});
-});
-
-beerRoute.get(function(req, res) {
-	Beer.findById(req.params.beer_id, function(err, beer) {
-		if(err) {
-			return res.send(err);
-		}
-		res.json(beer);
-	});
-});
-
-beerRoute.put(function(req, res) {
-	Beer.findById(req.params.beer_id, function(err, beer) {
-		if(err) {
-			return res.send(err);
-		}
-		
-		beer.quantity = req.body.quantity;
-		
-		beer.save(function(err) {
-			if(err) {
-				return res.send(err);
-			}
-			res.json(beer);
-		});
-	});
-});
-
-beerRoute.delete(function(req, res) {
-	Beer.findByIdAndRemove(req.params.beer_id, function(err) {
-		if(err) {
-			return res.send(err);
-		}
-
-		res.json({message: 'Beer removed from the locker!'});
-	});
-});
+router.route('/beers/:beer_id')
+	.get(beerController.getBeer)
+	.put(beerController.putBeer)
+	.delete(beerController.deleteBeer);
 
 app.use('/api', router);
-app.listen(port);
-console.log('Insert beer on port ' + port);
+app.listen(3000);
+
