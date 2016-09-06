80e177f71c7dc05e56448d05dd815df093b1fea3
sedat sevgili <sedatsevgili@gmail.com>
2016-09-06 18:48:54 +0300

    now beers have their own users!

--- controllers/beer.js
+++ controllers/beer.js
@@ -5,6 +5,7 @@ exports.postBeers = function(req, res) {
 	beer.name = req.body.name;
 	beer.type = req.body.type;
 	beer.quantity = req.body.quantity;
+	beer.userId = req.user._id;
 	
 	beer.save(function(err) {
 		if(err) {
@@ -15,7 +16,7 @@ exports.postBeers = function(req, res) {
 };
 
 exports.getBeers = function(req, res) {
-	Beer.find(function(err, beers) {
+	Beer.find({userId: req.user._id}, function(err, beers) {
 		if(err) {
 			return res.send(err);
 		}
@@ -24,7 +25,7 @@ exports.getBeers = function(req, res) {
 };
 
 exports.getBeer = function(req, res) {
-	Beer.findById(req.params.beer_id, function(err, beer) {
+	Beer.find({_id: req.params.beer_id, userId: req.user._id}, function(err, beer) {
 		if(err) {
 			return res.send(err);
 		}
@@ -33,7 +34,7 @@ exports.getBeer = function(req, res) {
 };
 
 exports.putBeer = function(req, res) {
-	Beer.findById(req.params.beer_id, function(err, beer) {
+	Beer.find({_id: req.params.beer_id, userId: req.user._id}, function(err, beer) {
 		if(err) {
 			return res.send(err);
 		}
@@ -50,10 +51,10 @@ exports.putBeer = function(req, res) {
 };
 
 exports.deleteBeer = function(req, res) {
-	Beer.findByIdAndRemove(req.params.beer_id, function(err) {
-		if(err) {
-			return res.send(err);
-		}
-		res.json({message: 'Beer removed from the locker!'});
+	Beer.remove({ userId: req.user._id, _id: req.params.beer_id }, function(err) {
+	    if (err)
+	      res.send(err);
+
+	    res.json({ message: 'Beer removed from the locker!' });
 	});
 };
