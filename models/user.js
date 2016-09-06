5a87425932885a5b6c3820922cd14de751c48f56
sedat sevgili <sedatsevgili@gmail.com>
2016-09-06 18:40:50 +0300

    basic authentication is on now!

--- models/user.js
+++ models/user.js
@@ -36,4 +36,13 @@ UserSchema.pre('save', function(callback) {
 	});
 });
 
+UserSchema.methods.verifyPassword = function(password, callback) {
+	bcrypt.compare(password, this.password, function(err, isMatch) {
+		if(err) {
+			return callback(err);
+		}
+		callback(null, isMatch);
+	});
+};
+
 module.exports = mongoose.model('User', UserSchema);
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var passport = require('passport');
 var beerController = require('./controllers/beer');
 var userController = require('./controllers/user');
+var authController = require('./controllers/auth');
 var app = express();
 var router = express.Router();
 
@@ -10,20 +12,22 @@ app.use(bodyParser.urlencoded({
 	extended: true
 }));
 
+app.use(passport.initialize());
+
 mongoose.connect('mongodb://localhost:27017/beerlocker');
 
 router.route('/beers')
-	.post(beerController.postBeers)
-	.get(beerController.getBeers);
+	.post(authController.isAuthenticated, beerController.postBeers)
+	.get(authController.isAuthenticated, beerController.getBeers);
 
 router.route('/beers/:beer_id')
-	.get(beerController.getBeer)
-	.put(beerController.putBeer)
-	.delete(beerController.deleteBeer);
+	.get(authController.isAuthenticated, beerController.getBeer)
+	.put(authController.isAuthenticated, beerController.putBeer)
+	.delete(authController.isAuthenticated, beerController.deleteBeer);
 
 router.route('/users')
 	.post(userController.postUsers)
-	.get(userController.getUsers);
+	.get(authController.isAuthenticated, userController.getUsers);
 
 app.use('/api', router);
 app.listen(3000);
