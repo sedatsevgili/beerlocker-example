e65bc09a03d63774cc58e417c2e994a831a0c6d2
sedat sevgili <sedatsevgili@gmail.com>
2016-09-07 06:22:17 +0300

    AuthCode and AccessToken models included and Bearer Strategy is implemented

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,8 @@
 var passport = require('passport');
 var BasicStrategy = require('passport-http').BasicStrategy;
 var User = require('../models/user');
+var BearerStrategy = require('passport-http-bearer').Strategy;
+var Token = require('../models/token');
 
 passport.use(new BasicStrategy(
 	function(username, password, callback) {
@@ -28,4 +30,46 @@ passport.use(new BasicStrategy(
 	}
 ));
 
+passport.use('client-basic', new BasicStrategy(
+	function(username, password, callback) {
+		Client.findOne({id: username}, function(err, client) {
+			if(err) {
+				return callback(err);
+			}
+			if(!client || client.secret != password) {
+				return callback(null, false);
+			}
+			return callback(null, true);
+		});
+	}
+));
+
+passport.use(new BearerStrategy({
+	function(accessToken, callback) {
+		Token.findOne({value: accessToken}, function(err, token) {
+			if(err) {
+				callback(err);
+			}
+
+			if(!token) {
+				return callback(null, false);
+			}
+
+			User.findOne({_id: token.userId}, function(err, user) {
+				if(err) {
+					return callback(err);
+				}
+
+				if(!user) {
+					return callback(null, false);
+				}
+
+				return callback(null, user, { scope: '*' });
+			});
+		});
+	}
+}));
+
 exports.isAuthenticated = passport.authenticate('basic', {session: false});
+exports.isClientAuthenticated = passport.authenticate('client-basic', {session: false});
+exports.isBearerAuthenticated = passport.authenticate('bearer', {session: false});
